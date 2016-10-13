1beb1f6fe60c3bdadb8d0b4bf1366ff4b4968b23
ravi kumar patel <ravikumarpatel44@gmail.com>
2016-10-14 01:21:31 +0530

    image slider

--- app/src/main/java/com/example/ravikumarpatel/gym_guide_app/MainActivity.java
+++ app/src/main/java/com/example/ravikumarpatel/gym_guide_app/MainActivity.java
@@ -13,17 +13,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     Button btnexercise,btnworkout;
 
+    Animation Fade_in, Fade_out;
+    ViewFlipper viewFlipper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewFlipper = (ViewFlipper) this.findViewById(R.id.flipper);
+        Fade_in = AnimationUtils.loadAnimation(this,android.R.anim.fade_in);
+        Fade_out = AnimationUtils.loadAnimation(this,android.R.anim.fade_out);
+
+        //Set out flipping
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.startFlipping();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
